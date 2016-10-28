63628dd5cdbaddbe7590249b85a607719ca556f7
FastDeath <FastDeath@users.noreply.github.com>
2016-10-28 18:01:08 +0200

    Fixed language recognition

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var releaseGroupPat = /-(\w+)$/;
 var tagSet = {
 	quality: ["HD", "SD", "HDTV", /\.\d{3,}p/g],
 	source: ["BluRay", "BDRip", "WEB-DL", "WEBRip", "iTunesHD", "AmazonHD"],
-	language: ["German", "English"],
+	languages: [/german/i, /english/i],
 	tags: [
 		/\.[xh]\.?264/i, "AVC",
 		/\.DD5\.?1/, "DTS", "AC3",
 		"Subbed", "Dubbed", "DL"
 	]
 };
+var languages = [/german/i, /english/i];
 
 exports.dissectTags = function (tagStr) {
 	tagStr = "." + tagStr + ".";
@@ -28,6 +29,21 @@ exports.dissectTags = function (tagStr) {
 	});
 
 	output.title = tagStr.replace(/\.+/g, " ").trim();
+
+	return output;
+}
+
+exports.dissectTitle = function (title, titleKey) {
+	var output = {};
+
+	/*output.languages = languages.map((language) => title.match(language))
+			.filter(m => !!m)
+			.map(m => (m instanceof Array) ? m : [m])
+			.reduce((res, matches) => res.concat(matches), [])
+			.map(m => m.replace(/(^\.+|\.+$)/g, "").toLowerCase());
+	languages.forEach((language) => title = title.replace(language, ""));*/
+
+	output[titleKey] = title.replace(/\.+/g, " ").trim();
 
 	return output;
 }
@@ -51,13 +67,12 @@ exports.dissectDirname = function (dirname, graceful) {
 	var series = /^(.*)\.S(\d+)(?:E(\d+))?\.(.*)$/i.exec(dirname);
 	if (series) {
 		output.type = (series[3]) ? "episode" : "season";
-		output.series = series[1].replace(/\.+/g, " ").trim();
 		output.season = parseInt(series[2]);
 		if (series[3])
-			output.series = parseInt(series[3]);
-		// { output.quality, output.source, output.tags } = dissectTags(series[4].trim("."));
+			output.episode = parseInt(series[3]);
+		Object.assign(output, exports.dissectTitle(series[1], "series"));
 		Object.assign(output, exports.dissectTags(series[4].trim(".")));
 	}
 
 	return output;
-};
+}
